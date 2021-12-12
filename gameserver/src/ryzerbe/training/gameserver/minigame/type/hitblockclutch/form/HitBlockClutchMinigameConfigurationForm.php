09e1cc9f992ca6e586526922ebd3ded702303394
Baubo-LP <53007287+Baubo-LP@users.noreply.github.com>
2021-12-12 18:53:59 +0100

    type player  name into seed input to get their seed

--- gameserver/src/ryzerbe/training/gameserver/minigame/type/hitblockclutch/form/HitBlockClutchMinigameConfigurationForm.php
+++ gameserver/src/ryzerbe/training/gameserver/minigame/type/hitblockclutch/form/HitBlockClutchMinigameConfigurationForm.php
@@ -19,11 +19,21 @@ class HitBlockClutchMinigameConfigurationForm {
         $form = new CustomForm(function(Player $player, mixed $data) use ($gameSession): void{
             if($data === null) return;
 
+            $seedPlayer = $player->getServer()->getPlayer($data["seed"]);
             $seed = match ($data["seed"]) {
                 "#only_one" => 16386391,
                 "#only_two" => 1826194,
                 default => intval($data["seed"])
             };
+            if($seedPlayer !== null) {
+                $seedPlayerSession = SessionManager::getInstance()->getSessionOfPlayer($seedPlayer);
+                if($seedPlayerSession === null) return;
+                $seedPlayerGameSession = $seedPlayerSession->getGameSession();
+                if($seedPlayerGameSession instanceof HitBlockClutchGameSession) {
+                    $seed = $seedPlayerGameSession->getSeed();
+                }
+            }
+
             if($gameSession->getSeed() !== $seed) {
                 $gameSession->resetTimer("default", true);
                 $gameSession->setSeed($seed);
@@ -34,7 +44,7 @@ class HitBlockClutchMinigameConfigurationForm {
             $gameSession->resetGame();
         });
         $form->setTitle($gameSession->getSettings()->PREFIX);
-        $form->addInput("§cSeed", "", strval($gameSession->getSeed()), "seed");
+        $form->addInput("§cSeed (or another playername to get their seed)", "", strval($gameSession->getSeed()), "seed");
         $form->sendToPlayer($player);
     }
 }
