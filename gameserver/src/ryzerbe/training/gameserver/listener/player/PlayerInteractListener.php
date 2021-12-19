31847a0bf9cfe7aadd0fd3aeb8f0ec50fd047d2d
Baubo-LP <53007287+Baubo-LP@users.noreply.github.com>
2021-12-19 13:07:04 +0100

    mobiles doesn't get a bowl if he soup

--- gameserver/src/ryzerbe/training/gameserver/listener/player/PlayerInteractListener.php
+++ gameserver/src/ryzerbe/training/gameserver/listener/player/PlayerInteractListener.php
@@ -2,13 +2,17 @@
 
 namespace ryzerbe\training\gameserver\listener\player;
 
+use pocketmine\block\BlockIds;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerInteractEvent;
 use pocketmine\item\Item;
 use pocketmine\item\ItemIds;
 use pocketmine\Player;
+use ryzerbe\core\player\data\LoginPlayerData;
+use ryzerbe\core\player\RyZerPlayerProvider;
 use ryzerbe\training\gameserver\minigame\MinigameManager;
 use ryzerbe\training\gameserver\minigame\type\kitpvp\KitPvPMinigame;
+use function in_array;
 
 class PlayerInteractListener implements Listener {
 
@@ -22,6 +26,10 @@ class PlayerInteractListener implements Listener {
         $minigame = MinigameManager::getMinigameByPlayer($player);
         if($minigame === null) {
             if($player->isCreative()) return;
+            $event->setCancelled();
+            return;
+        }
+        if($player->isSpectator()){
             $event->setCancelled();
             return;
         }
@@ -42,8 +50,14 @@ class PlayerInteractListener implements Listener {
 
         if($item->getId() === ItemIds::MUSHROOM_STEW && $minigame instanceof KitPvPMinigame) {
             if((int)$player->getHealth() === $player->getMaxHealth()) return;
+            $rbePlayer = RyZerPlayerProvider::getRyzerPlayer($player);
             $player->setHealth($player->getHealth() + 4.5);
-            $player->getInventory()->setItemInHand(Item::get(ItemIds::BOWL));
+            if($rbePlayer === null) {
+                $player->getInventory()->setItemInHand(Item::get(ItemIds::BOWL));
+            }else {
+                $os = [LoginPlayerData::TOUCH, LoginPlayerData::CONTROLLER];
+                $player->getInventory()->setItemInHand(in_array($rbePlayer->getLoginPlayerData()->getCurrentInputMode(), $os) ? Item::get(BlockIds::AIR) : Item::get(ItemIds::BOWL));
+            }
             $player->playSound("random.eat", 2.0, 1.0, [$player]);
         }
     }
